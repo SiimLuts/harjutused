c3a9915d05ec470294064d103c20520c51fac643
SiimLuts <siim.luts@tptlive.ee>
2022-09-21 14:42:55 +0300

    tegin todo

--- hajusrakendused/todo-frontend/src/components/views/TaskList.jsx
+++ hajusrakendused/todo-frontend/src/components/views/TaskList.jsx
@@ -1,14 +1,26 @@
 import { DeleteOutlined } from "@ant-design/icons";
 import { Input, Button, Checkbox, List, Col, Row, Space, Divider } from "antd";
 import produce from "immer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function TaskList() {
     const [tasks, setTasks] = useState([
-        {id: 1, name: "Task 1", completed: false},
-        {id: 2, name: "Task 2", completed: true},
+        { id: 1, title: "Task 1", completed: false },
+        { id: 2, title: "Task 2", completed: true },
     ]);
+
+    useEffect(() => {
+        console.log("useEffect")
+        fetch('http://demo2.z-bit.ee/tasks', {
+            method: "GET",
+            headers: {
+                'Authorization': 'Bearer ' + "zRs_JiJBjJgBWlD43Ll3yygIXolXrsnF",
+                'Content-Type': 'application/json'
+            },
+        }).then(res => res.json())
+            .then(res => setTasks(res))
+    }, []);
 
     const handleNameChange = (task, event) => {
         console.log(event)
@@ -29,13 +41,30 @@ export default function TaskList() {
     };
 
     const handleAddTask = () => {
+        const newTask = {
+            id: Math.random(),
+            name: "",
+            title: "test123",
+            completed: false
+
+        };
+
+
         setTasks(produce(tasks, draft => {
-            draft.push({
-                id: Math.random(),
-                name: "",
-                completed: false
-            });
+            draft.push(newTask)
+
         }));
+        fetch('http://demo2.z-bit.ee/tasks', {
+            method: "POST",
+            headers: {
+                'Authorization': 'Bearer ' + "zRs_JiJBjJgBWlD43Ll3yygIXolXrsnF",
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newTask)
+        }).then(res => res.json())
+            .then(res => console.log(res))
+
+
     };
 
     const handleDeleteTask = (task) => {
@@ -46,7 +75,7 @@ export default function TaskList() {
     };
 
     return (
-        <Row type="flex" justify="center" style={{minHeight: '100vh', marginTop: '6rem'}}>
+        <Row type="flex" justify="center" style={{ minHeight: '100vh', marginTop: '6rem' }}>
             <Col span={12}>
                 <h1>Task List</h1>
                 <Button onClick={handleAddTask}>Add Task</Button>
@@ -56,10 +85,10 @@ export default function TaskList() {
                     bordered
                     dataSource={tasks}
                     renderItem={(task) => <List.Item key={task.id}>
-                        <Row type="flex" justify="space-between" align="middle" style={{width: '100%'}}>
+                        <Row type="flex" justify="space-between" align="middle" style={{ width: '100%' }}>
                             <Space>
                                 <Checkbox checked={task.completed} onChange={(e) => handleCompletedChange(task, e)} />
-                                <Input value={task.name} onChange={(event) => handleNameChange(task, event)} />
+                                <Input value={task.title} onChange={(event) => handleNameChange(task, event)} />
                             </Space>
                             <Button type="text" onClick={() => handleDeleteTask(task)}><DeleteOutlined /></Button>
                         </Row>
